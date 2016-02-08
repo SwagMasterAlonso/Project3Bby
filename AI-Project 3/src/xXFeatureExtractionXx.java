d527fb839e234355f4a323e62ce550e7167463bf
alonso_martinez <almartnz@gmail.com>
2016-02-08 18:27:19 -0500

    Fuck you.

--- AI-Project 3/src/xXFeatureExtractionXx.java
+++ AI-Project 3/src/xXFeatureExtractionXx.java
@@ -5,13 +5,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
 
 
 
